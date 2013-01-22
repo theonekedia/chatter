0da966418377021eb7dea8473079f5c4e0ed066d
theone <theone.kedia@gmail.com>
2013-01-22 14:36:50 +0530

    Create user resource

---
+++ db/migrate/20130122085510_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :digest
+      t.string :avatar
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
