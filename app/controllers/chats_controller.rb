41741eb5937361a9e9e2ddbe664fbe91166e9fb0
theone <theone.kedia@gmail.com>
2013-01-22 17:16:29 +0530

    added the public chats page

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,17 @@
 class ChatsController < ApplicationController
 	def create
 		@chat = Chat.new(params[:chat])
-		@chat.userid = current_user.id
+		@chat.user_id = current_user.id
 		if @chat.save
 			redirect_to current_user
 		else
 			flash[:error] = "Problem!"
 			redirect_to current_user
 		end
+	end
+
+	def index
+		@chats = Chat.all include: :user
+		@chat = Chat.new		
 	end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,5 @@
 class Chat < ActiveRecord::Base
-  default_scope order: 'createdat DESC'
+  default_scope order: 'created_at DESC'
   attr_accessible :content, :user_id
   belongs_to :user
   validates :content, length: { maximum: 140 }
